b1afbb3336d1c4c3c7e5e8c231316276b53460f1
tuilakie <ntneik15@gmail.com>
2021-10-18 12:08:25 +0700

    Initial commit

---
+++ src/Exercise01.java
@@ -0,0 +1,14 @@
+/**
+ * PACKAGE_NAME
+ * Created by Nguyen Thanh Kien 19127456
+ * Date 18/10/2021 - 11:49 SA
+ * Description: ...
+ */
+public class Exercise01 {
+    public static void main(String[] args) {
+
+    }
+}
+class MangMotChieu {
+
+}
